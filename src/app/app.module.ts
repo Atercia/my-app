3585d8be34737c47c4f7fce9c1880760f7d1a588
Atercia <34149452+Atercia@users.noreply.github.com>
2019-03-03 11:28:47 +0800

    c-menu component test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CMenuComponent } from './c-menu/c-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/c-menu/c-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-menu',
+  templateUrl: './c-menu.component.html',
+  styleUrls: ['./c-menu.component.scss']
+})
+export class CMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
